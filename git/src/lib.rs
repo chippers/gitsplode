7f23668b4f803860880e61531270769e0bb5d62b
chip <chip@chip.sh>
2020-01-16 14:03:33 -0800

    add version to git lib

--- git/src/lib.rs
+++ git/src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use git_sys::git_version_string;
+use std::ffi::CStr;
+use std::sync::Once;
+
+static INIT_VERSION: Once = Once::new();
+static mut VERSION: &str = "";
+pub fn version() -> &'static str {
+    unsafe {
+        INIT_VERSION.call_once(|| {
+            VERSION = CStr::from_ptr(git_version_string.as_ptr())
+                .to_str()
+                .expect("invalid utf-8 found in git version string")
+        });
+
+        VERSION
     }
 }

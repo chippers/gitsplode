827effaa0124aee816b8e9a5379694ad717da43f
chip <chip@chip.sh>
2020-01-21 16:47:26 -0800

    add potentially unsound singleton. verify later

--- git/src/lib.rs
+++ git/src/lib.rs
@@ -2,6 +2,30 @@ use git_sys::git_version_string;
 use std::ffi::CStr;
 use std::sync::Once;
 
+pub struct Git {
+    repository: *mut git_sys::repository,
+}
+
+static INIT_GIT: Once = Once::new();
+static mut GIT: Git = Git {
+    repository: std::ptr::null_mut(),
+};
+
+/// FIXME: is this unsafe? essentially potentially exposing a static !Send ptr across threads
+pub fn init() -> &'static Git {
+    unsafe {
+        INIT_GIT.call_once(|| {
+            git_sys::git_setup_gettext();
+            git_sys::attr_start();
+            git_sys::initialize_the_repository();
+
+            GIT.repository = git_sys::the_repository;
+        });
+
+        &GIT
+    }
+}
+
 static INIT_VERSION: Once = Once::new();
 static mut VERSION: &str = "";
 pub fn version() -> &'static str {
